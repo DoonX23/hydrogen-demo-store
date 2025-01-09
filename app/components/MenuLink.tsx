4f8f55c975fcb8d5f71a9376cc9280202556da5f
DoonX23 <alancooper4u@gmail.com>
2025-01-09 19:45:54 +0800

    因为原生item.to只有最后面的handle，所以用完整的item.url去掉域名的方式获得完整路径

--- app/components/MenuLink.tsx
+++ app/components/MenuLink.tsx
@@ -27,9 +27,18 @@ export function MenuLink({
       </a>
     );
   }
+
+    // 处理内部链接
+    let path = item.to;
+    if (item.url) {
+      // 移除域名部分，只保留路径
+      const url = new URL(item.url);
+      path = url.pathname; // 这会保留完整的路径，如 /materials/Url1/Url2
+    }
+
   return (
     <Link 
-      to={item.to} 
+      to={path} 
       target={item.target} 
       onClick={onClose} 
       prefetch="intent"
