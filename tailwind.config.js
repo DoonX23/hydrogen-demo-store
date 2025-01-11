79ea0f3b6b88d1f4a6479a039c3c36ba6f4e52d5
DoonX23 <alancooper4u@gmail.com>
2025-01-11 23:42:26 +0800

    引入了defalultTheme，这样可以修改覆盖

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 import formsPlugin from '@tailwindcss/forms';
 import typographyPlugin from '@tailwindcss/typography';
 
+//可以基于defaultTheme的预设参数去修改覆盖；
+import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    //修改container的预设，注意它不单是个类名
+    container: {
+      center: true,
+      darkMode: 'false',
+      padding: {
+        DEFAULT: '1rem',
+        xl: '40px',
+        '2xl': '128px',
+      },
+    },
     extend: {
       colors: {
         primary: 'rgb(var(--color-primary) / <alpha-value>)',
@@ -15,17 +27,28 @@ export default {
         highlight: 'rgb(var(--color-highlight) / <alpha-value>)',
       },
       screens: {
-        sm: '32em',
-        md: '48em',
-        lg: '64em',
-        xl: '80em',
-        '2xl': '96em',
-        'sm-max': {max: '48em'},
-        'sm-only': {min: '32em', max: '48em'},
-        'md-only': {min: '48em', max: '64em'},
-        'lg-only': {min: '64em', max: '80em'},
-        'xl-only': {min: '80em', max: '96em'},
-        '2xl-only': {min: '96em'},
+        // 引入defaultTheme是因为: hydrogen-demo-store模板把max-width去掉了，导致无论哪种设备左右边距都是固定的。
+        // sm: '32em',
+        // md: '48em',
+        // lg: '64em',
+        // xl: '80em',
+        // '2xl': '96em',
+        // 'sm-max': {max: '48em'},
+        // 'sm-only': {min: '32em', max: '48em'},
+        // 'md-only': {min: '48em', max: '64em'},
+        // 'lg-only': {min: '64em', max: '80em'},
+        // 'xl-only': {min: '80em', max: '96em'},
+        // '2xl-only': {min: '96em'},
+        ...defaultTheme.screens,
+        'sm-max': {max: defaultTheme.screens.md},
+        'sm-only': {min: defaultTheme.screens.sm, max: defaultTheme.screens.md},
+        'md-only': {min: defaultTheme.screens.md, max: defaultTheme.screens.lg},
+        'lg-only': {min: defaultTheme.screens.lg, max: defaultTheme.screens.xl},
+        'xl-only': {
+          min: defaultTheme.screens.xl,
+          max: defaultTheme.screens['2xl'],
+        },
+        '2xl-only': {min: defaultTheme.screens['2xl']},
       },
       spacing: {
         nav: 'var(--height-nav)',
@@ -54,7 +77,6 @@ export default {
       maxWidth: {
         'prose-narrow': '45ch',
         'prose-wide': '80ch',
-        'custom': '1580px',
       },
       boxShadow: {
         border: 'inset 0px 0px 0px 1px rgb(var(--color-primary) / 0.08)',
