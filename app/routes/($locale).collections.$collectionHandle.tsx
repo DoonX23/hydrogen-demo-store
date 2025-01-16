3f1fdea6c56c16ceb95d9ace5132b6a30608098a
DoonX23 <alancooper4u@gmail.com>
2025-01-16 19:40:09 +0800

    增加目录页面产品数量为250

--- app/routes/($locale).collections.$collectionHandle.tsx
+++ app/routes/($locale).collections.$collectionHandle.tsx
@@ -36,7 +36,7 @@ export const headers = routeHeaders;
 
 export async function loader({params, request, context}: LoaderFunctionArgs) {
   const paginationVariables = getPaginationVariables(request, {
-    pageBy: 8,
+    pageBy: 250,
   });
   const {collectionHandle} = params;
   const locale = context.storefront.i18n;
