ca57e4c0e4abd64fb5567b7015198904ba728a12
DoonX23 <alancooper4u@gmail.com>
2025-02-15 20:57:45 +0800

    修改banner组件样式；

--- app/components/SiteFeatures.tsx
+++ app/components/SiteFeatures.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowPathIcon, CalendarIcon, TruckIcon, ShieldCheckIcon } from '@heroicons/react/24/outline';
+import { CurrencyDollarIcon, ChatBubbleBottomCenterTextIcon, TruckIcon, ShieldCheckIcon } from '@heroicons/react/24/solid';
 
 interface Feature {
   name: string;
@@ -8,22 +8,22 @@ interface Feature {
 }
 
 const features: Feature[] = [
-  { name: '10-year all-inclusive warranty', description: "We'll replace it with a new one", icon: CalendarIcon },
-  { name: 'Free shipping on returns', description: 'Send it back for free', icon: ArrowPathIcon },
-  { name: 'Free, contactless delivery', description: 'The shipping is on us', icon: TruckIcon },
-  { name: '100% Secure Checkout', description: 'PayPal / MasterCard / Visa', icon: ShieldCheckIcon },
+  { name: 'INSTANT', description: "cut to size prices", icon: CurrencyDollarIcon },
+  { name: 'GLOBAL DELIVERY', description: 'Ships to 200+ countries', icon: TruckIcon },
+  { name: 'OUTSTANDING', description: 'customer service', icon: ChatBubbleBottomCenterTextIcon },
+  { name: 'SECURE CHECKOUT', description: 'PayPal / MasterCard / Visa', icon: ShieldCheckIcon },
 ];
 
 export const SiteFeatures: React.FC = () => {
     return (
-      <section className="container">
-        <div className="mx-auto py-12">
+      <section className="bg-blue-200 dark:bg-brand">
+        <div className="container mx-auto py-6">
           <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8">
             {features.map((feature, index) => (
               <div key={index} className="flex items-start">
                 <div className="flex-shrink-0">
                   <div className="flex h-12 w-12 items-center justify-center rounded-md bg-brand-100 dark:bg-brand-900">
-                    <feature.icon className="h-6 w-6 text-brand-600 dark:text-white" aria-hidden="true" />
+                    <feature.icon className="h-8 w-8 text-highlight" aria-hidden="true" />
                   </div>
                 </div>
                 <div className="ml-4">
