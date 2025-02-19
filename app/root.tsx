c9cbc2fad066348f681a2f8d70349f67635c7ea7
DoonX23 <alancooper4u@gmail.com>
2025-02-20 03:17:00 +0800

    把gtm代码中的创建datalayer和创建注入新脚本拆分成2个Script组件，一个内联脚本不影响dom，另一个外联脚本等水合结束再创建注入新脚本。

--- app/root.tsx
+++ app/root.tsx
@@ -146,13 +146,21 @@ function Layout({children}: {children?: React.ReactNode}) {
         <meta name="msvalidate.01" content="A352E6A0AF9A652267361BBB572B8468" />
         <Meta />
         <Links />
-        <Script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NT3BGD4');`,
-        }}></Script>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              dataLayer.push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+              });
+            `
+          }}
+        />
+        <Script
+          waitForHydration
+          src={`https://www.googletagmanager.com/gtm.js?id=GTM-NT3BGD4`}
+        />
       </head>
       <body>
         <noscript>
