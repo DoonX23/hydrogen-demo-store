7903c26e6e5fb9887fa5ce43196aae962019d0d0
DoonX23 <alancooper4u@gmail.com>
2025-02-19 22:56:05 +0800

    移除gtm，进行测试

--- app/root.tsx
+++ app/root.tsx
@@ -146,26 +146,10 @@ function Layout({children}: {children?: React.ReactNode}) {
         <meta name="msvalidate.01" content="A352E6A0AF9A652267361BBB572B8468" />
         <Meta />
         <Links />
-        <Script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NT3BGD4');`,
-        }}></Script>
+
       </head>
       <body>
-        <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NT3BGD4"
-              height="0"
-              width="0"
-              style={{
-                display: 'none',
-                visibility: 'hidden'
-              }}
-            ></iframe>
-          </noscript>
+
         {data ? (
           <Analytics.Provider
             cart={data.cart}
@@ -178,7 +162,7 @@ function Layout({children}: {children?: React.ReactNode}) {
             >
               {children}
             </PageLayout>
-            <GoogleTagManager />
+
           </Analytics.Provider>
         ) : (
           children
