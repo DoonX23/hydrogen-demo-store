3956ddbc0859644f873102e40114c43a2e35854b
DoonX23 <alancooper4u@gmail.com>
2025-02-20 13:31:03 +0800

    修改内容

--- app/components/Header/Banner.tsx
+++ app/components/Header/Banner.tsx
@@ -5,12 +5,12 @@ export function Banner() {
     <div className="bg-highlight sticky z-10 h-16 text-white xl:h-10 top-0 xl:w-full">
       <div className="container mx-auto px-4 xl:h-full py-2 xl:py-0">
         <div className="flex flex-col items-center xl:flex-row xl:justify-center xl:h-full text-sm gap-1 xl:gap-2">
-          <div>Ordering over $199?</div>
+          <div>Up to 50% Off orders $199+ with custom quotes.</div>
           <Link
-            to="/pages/contact"
+            to="/pages/contact-doonx"
             className="border-b border-transparent hover:border-white transition-colors"
           >
-            Contact Us for a discounted price
+            Contact Us
           </Link>
         </div>
       </div>
