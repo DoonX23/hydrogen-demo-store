adc665bbe1a0bd7c337f3f4c132baffe40aedbe1
DoonX23 <alancooper4u@gmail.com>
2025-03-27 22:26:48 +0800

    修改样式

--- app/components/ProductDescriptionSection.tsx
+++ app/components/ProductDescriptionSection.tsx
@@ -22,20 +22,17 @@ export function ProductDescriptionSection({ product }: { product: ProductQuery['
     const filteredAttributes = productAttributes.filter(attr => attr.value);
 
     return (
-        <div id="description" className="py-8">
+        <div id="description" className="py-8 md:py-12">
             <h2 className="text-2xl font-semibold">Product Details</h2>
             <div className="flex flex-col md:flex-row">
                 {/* 产品属性区块 */}
                 <div className="pb-4 w-full md:w-1/2">
-                    <div className="px-4 sm:px-0">
-                        <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">Personal details and application.</p>
-                    </div>
                     <div className="mt-6 border-t border-gray-100">
                         <dl className="divide-y divide-gray-100">
                             {filteredAttributes.map((attr, index) => (
                                 <div
                                     key={attr.name}
-                                    className={`${index % 2 === 0 ? 'bg-blue-100' : 'bg-white'} px-4 py-3 grid grid-cols-2 gap-4 sm:px-3`}
+                                    className={`${index % 2 === 0 ? 'bg-blue-100' : 'bg-white'} px-4 py-2 grid grid-cols-2 gap-4 sm:px-3`}
                                 >
                                     <dt className="text-sm/6 font-medium text-gray-900">{attr.name}</dt>
                                     <dd className="text-sm/6 text-gray-700">{attr.value}</dd>
@@ -44,15 +41,12 @@ export function ProductDescriptionSection({ product }: { product: ProductQuery['
                         </dl>
                     </div>
                 </div>
-
-                {/* 图片占位区块 */}
-                <div className="w-full md:w-1/2 p-10 flex items-center justify-center" />
             </div>
 
             {/* 产品描述 */}
             {!!descriptionHtml && (
                 <div
-                    className="prose prose-sm sm:prose dark:prose-invert sm:max-w-4xl mt-7"
+                    className="prose prose-sm sm:prose dark:prose-invert sm:max-w-4xl my-7"
                     dangerouslySetInnerHTML={{ __html: descriptionHtml || '' }}
                 />
             )}
@@ -61,7 +55,7 @@ export function ProductDescriptionSection({ product }: { product: ProductQuery['
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {/* 特色部分 */}
                 {product.features?.value && (
-                    <div className="bg-gray-50 p-4 rounded">
+                    <div className="bg-gray-50 pb-3 rounded">
                         <h3 className="font-bold pb-4">Features</h3>
                         <RichText
                             data={product.features.value}
@@ -71,7 +65,7 @@ export function ProductDescriptionSection({ product }: { product: ProductQuery['
                                     node.listType === "unordered"
                                         ? <ul className="custom-list">{node.children}</ul>
                                         : <ol className="custom-list">{node.children}</ol>,
-                                listItem: ({ node }) => <li className="custom-list-item">{node.children}</li>
+                                listItem: ({ node }) => <li className="relative pl-6 before:content-[''] before:absolute before:left-0 before:top-[0.6em] before:h-1.5 before:w-1.5 before:rounded-full before:bg-brand">{node.children}</li>
                             }}
                         />
                     </div>
@@ -79,7 +73,7 @@ export function ProductDescriptionSection({ product }: { product: ProductQuery['
 
                 {/* 应用部分 */}
                 {product.applications?.value && (
-                    <div className="bg-gray-50 p-4 rounded">
+                    <div className="bg-gray-50 pb-3 rounded">
                         <h3 className="font-bold pb-4">Applications</h3>
                         <RichText
                             data={product.applications.value}
@@ -89,7 +83,7 @@ export function ProductDescriptionSection({ product }: { product: ProductQuery['
                                     node.listType === "unordered"
                                         ? <ul className="custom-list">{node.children}</ul>
                                         : <ol className="custom-list">{node.children}</ol>,
-                                listItem: ({ node }) => <li className="custom-list-item">{node.children}</li>
+                                listItem: ({ node }) => <li className="relative pl-6 before:content-[''] before:absolute before:left-0 before:top-[0.6em] before:h-1.5 before:w-1.5 before:rounded-full before:bg-brand">{node.children}</li>
                             }}
                         />
                     </div>
