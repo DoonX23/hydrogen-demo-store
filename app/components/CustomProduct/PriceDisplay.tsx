689791d9427d4071d6244d625d79c453c180bdbb
DoonX23 <alancooper4u@gmail.com>
2025-11-26 14:15:32 +0800

    增加Buy more, save more

--- app/components/CustomProduct/PriceDisplay.tsx
+++ app/components/CustomProduct/PriceDisplay.tsx
@@ -5,6 +5,26 @@ import {
 } from '~/utils/calculations';
 
 /**
+ * 折扣标签图标
+ */
+function DiscountIcon() {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      className="h-3.5 w-3.5 mr-1 text-highlight" 
+      viewBox="0 0 20 20" 
+      fill="currentColor"
+    >
+      <path 
+        fillRule="evenodd" 
+        d="M17.707 9.293a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-7-7A.997.997 0 012 10V5a3 3 0 013-3h5c.256 0 .512.098.707.293l7 7zM5 6a1 1 0 100-2 1 1 0 000 2z" 
+        clipRule="evenodd" 
+      />
+    </svg>
+  );
+}
+
+/**
  * PriceDisplay组件 - 用于显示产品的价格和优惠码
  */
 export function PriceDisplay(props: CalculationProps) {
@@ -61,6 +81,11 @@ export function PriceDisplay(props: CalculationProps) {
         Price:
         <span className="text-brand dark:text-highlight"> ${formattedPrice}</span>
         <span className="text-brand dark:text-highlight text-sm font-normal ml-2">/each</span>
+        {/* 添加买多更便宜的小字提示 */}
+        <span className="inline-flex items-center text-xs text-gray-500 dark:text-gray-400 ml-3 font-normal">
+        <DiscountIcon />
+          Buy more, save more
+        </span>
       </div>
       {/*
       <div className="mt-4 p-3 border border-blue-100 rounded bg-blue-100 text-highlight">
